49d41cd3efc973e9522ad628bc750af541ad049a
jiangnan8118 <543455483@qq.com>
2022-09-13 23:43:24 +0800

    Solved this question
    It is about ASCII and use ASCII to solve it. And have some judgement.

--- Question.cpp
+++ Question.cpp
@@ -3,23 +3,54 @@
 /*
 	STEP 1:
 	Try to solve a question about a char
-	
+	STEP 2:
+	To solve for circle
+	STEP 3:
+	Improve program
 */
 int main()
 {
+	int n;	//read sum of number
 	char word;	//to read input
-	bool numState;	//to record wheather input number
+	int number;	//to record number
+	bool numState = false;	//to record wheather input number
+
+	scanf("%1d", &n);	//to read n
 	
-	scanf("%c", &word);
-	if (word>='a'&&word<='z'||word>='A'&&word<='Z')	//to judge word is a letter
+	for (;n>0;n--)
 	{
-		printf("This is a letter!");	//test message
+		scanf("%c", &word);
+		if (word>='a'&&word<='z' || word>='A'&&word<='Z')	//to judge word is a letter
+		{
+			//printf("This is a letter!\n");	//test message
+			if (word>='a' && word<='z'){
+				word = word - 32;	//a --> A
+			}else{
+				word = word + 32;	//A --> a
+			}
+			
+			if (numState){	//more letter?
+				
+				for(;number>0;number--){	//
+				printf("%c", word);
+				}
+				printf("\n");
+				numState = false;
+				
+			}else{
+				printf("%c\n", word);	//
+			}
+		}
+		else if (word>='0'&&word<='9')	//word is number?
+		{
+			//printf("This is a number!\n");	//test message
+			number = word - '0';	//calculate number
+			numState = true;	//set numState
+		}
+		else {
+		//printf("This isn't a letter or number!\n");	//test message
+		printf("special letter!\n");	//
+		}
 	}
-	else if (word>='0'&&word<='9')
-	{
-		printf("This is a number!");	//test message
-	}
-	else 
-	printf("This isn't a letter or number!");	//test message
 	
 }
