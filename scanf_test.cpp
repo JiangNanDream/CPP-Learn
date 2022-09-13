06740ed552e456e7c3daf38dfa35f59624ab91aa
jiangnan8118 <543455483@qq.com>
2022-09-13 23:44:57 +0800

    Create scanf_test.cpp
    try scanf

---
+++ scanf_test.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	/***
+	char word;
+	scanf("%c", &word);
+	printf("%c", word);
+	return 0;
+	***/
+	
+	
+	char a, b, c;
+	scanf("%c %c %c", &a, &b, &c);
+	int i;
+	i = a;
+	printf("%c %c %c", a, b, c);
+	return 0;
+
+}
